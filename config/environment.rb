3a2e98731c92261ab87555b93ebbd95b4a863674
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-18 16:41:29 -0500

    Migrated users table and added a root route for devise setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+ #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 # Initialize the Rails application.
 Rails.application.initialize!
