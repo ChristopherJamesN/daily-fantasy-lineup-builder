fbc86aaecc6404eb12a4009d194cc32bda952ec7
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-19 19:44:33 -0500

    Update devise route to accept callback from Facebook and add OmniauthCallbacksController to handle the callback action.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :lineups do
     resources :players, only: [:index, :new]
   end
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get '/users/show', to: 'users#show'
   root to: "lineups#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
