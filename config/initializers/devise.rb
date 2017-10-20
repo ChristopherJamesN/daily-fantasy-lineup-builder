31b0902e528940e4b15897016be1b85b2e1703c5
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-19 19:35:53 -0500

    Updated user model and devise config file to accept facebook authorization.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,6 +16,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
+  config.omniauth :facebook, "APP_ID", "APP_SECRET"
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
