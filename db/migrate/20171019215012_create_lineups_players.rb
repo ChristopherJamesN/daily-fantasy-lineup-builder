c83b3c32b94f59f7e6d331d9b0c2e36a9c8ee4e6
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-26 13:47:54 -0500

    Changed player comment column to player starting column.

--- db/migrate/20171019215012_create_lineups_players.rb
+++ db/migrate/20171019215012_create_lineups_players.rb
@@ -3,7 +3,7 @@ class CreateLineupsPlayers < ActiveRecord::Migration[5.1]
     create_table :lineups_players do |t|
       t.integer :lineup_id
       t.integer :player_id
-      t.string :player_comment
+      t.boolean :player_starting, default: true
 
     end
   end
