ed274a0dd68ac48d616447772ce31976d3d47048
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-26 16:25:07 -0500

    Changed player_starting column in lineups_players table to a string value.

--- db/migrate/20171019215012_create_lineups_players.rb
+++ db/migrate/20171019215012_create_lineups_players.rb
@@ -3,7 +3,7 @@ class CreateLineupsPlayers < ActiveRecord::Migration[5.1]
     create_table :lineups_players do |t|
       t.integer :lineup_id
       t.integer :player_id
-      t.boolean :player_starting, default: true
+      t.string :player_starting
 
     end
   end
