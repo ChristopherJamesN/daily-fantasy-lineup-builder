35bb4a56aea4ec30a2189ab7ea3d1a879da0209e
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-26 22:50:16 -0500

    Updated edit player page to show lineup and associated player status.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,19 @@
 class Player < ApplicationRecord
   validates :name, presence: true
   validates :position, presence: true
-  
+
   has_many :lineups_players
   has_many :lineups, through: :lineups_players
 
   accepts_nested_attributes_for :lineups
   accepts_nested_attributes_for :lineups_players
+
+  def lineups_players_attributes=(lineups_player_attributes)
+    lineups_player_attributes.values.each do |lineups_player_attribute|
+      if LineupsPlayer.find_by(lineup_id: lineups_player_attribute[:lineup_id], player_id: self.id)
+        LineupsPlayer.find_by(lineup_id: lineups_player_attribute[:lineup_id], player_id: self.id).update(player_starting: lineups_player_attribute[:player_starting])
+      end
+    end
+  end
 
 end
