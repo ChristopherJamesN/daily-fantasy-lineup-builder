706bda365c553b19cd6f5893f60d9fc0720da8dd
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-31 13:15:02 -0500

    Update showLineups function to fix syntax errors.

--- app/assets/javascripts/lineupshow.js
+++ app/assets/javascripts/lineupshow.js
@@ -1,13 +1,13 @@
 function showLineups() {
-    $.ajax({
+   $.ajax({
       method: 'GET',
       url: '/lineups.json',
       success: function(response) {
-        let new_html = response
+        let new_html = ''
         for (var i = 0; i < response.length; i++) {
           new_html += response[i].name + '<br>' + response[i].description + '</br>'
         }
         window.document.getElementById('lineups').innerHTML = new_html;
       }
     });
-  });
+  }
