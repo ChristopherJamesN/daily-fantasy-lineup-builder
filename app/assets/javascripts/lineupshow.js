986078d80bbfaeeb56330732dc96790af49ca986
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-31 15:00:32 -0500

    Updated lineup show method to iterate through all players in a lineup and disply their information.

--- app/assets/javascripts/lineupshow.js
+++ app/assets/javascripts/lineupshow.js
@@ -5,7 +5,11 @@ function showLineups() {
       success: function(response) {
         let new_html = ''
         for (var i = 0; i < response.data.length; i++) {
-          new_html += response.data[i].attributes.name + '<br>' + response.data[i].attributes.description + '</br>' + response.data[i].attributes.players[0].name + '<br>'
+          let player_html = ''
+            for (var j = 0; j < response.data[i].attributes.players.length; j++) {
+              player_html += response.data[i].attributes.players[j].name + ' ' + response.data[i].attributes.players[j].position +'<br>'
+            }
+          new_html += response.data[i].attributes.name + '<br>' + response.data[i].attributes.description + '<br>' + player_html + '<br>'
         }
         window.document.getElementById('lineups').innerHTML = new_html;
       }
