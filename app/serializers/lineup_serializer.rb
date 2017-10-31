6608ee14443406969b211ea422732ac15d8ee02c
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-31 14:10:36 -0500

    Updated associations between lineup serializer and player serializer, remove lineup_id from _player.json.jbuilder.

--- app/serializers/lineup_serializer.rb
+++ app/serializers/lineup_serializer.rb
@@ -1,3 +1,4 @@
 class LineupSerializer < ActiveModel::Serializer
   attributes :id, :name, :description
+  has_many :players
 end
--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,3 +1,4 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :name, :position, :projectedPoints, :actualPoints
+  belongs_to :lineup
 end
