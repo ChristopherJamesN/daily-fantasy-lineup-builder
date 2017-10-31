98589e4f24c05dab8164bfc26604ef75fb739827
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-31 16:37:30 -0500

    Update lineups show page and lineup show function to properly increment the lineup id.

--- app/assets/javascripts/nextLineup.js
+++ app/assets/javascripts/nextLineup.js
@@ -1,5 +1,5 @@
-function nextLineup(id) {
-  var nextId = parseInt(id) + 1
+function nextLineup() {
+  var nextId = parseInt(window.document.getElementById('next').getAttribute('data-id')) + 1
   $.ajax({
     method: 'GET',
     url: '/lineups/' + nextId + '.json',
@@ -7,6 +7,7 @@ function nextLineup(id) {
       window.document.getElementById('name').innerHTML = response.data.attributes.name
       window.document.getElementById('description').innerHTML = response.data.attributes.description
       window.document.getElementById('user').innerHTML = response.data.attributes.user.id
+      window.document.getElementById('next').setAttribute('data-id', response.data.id)
     }
   });
 }
