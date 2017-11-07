bc2093c457468ce9831a277cf50e79537a8216f4
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-07 08:25:14 -0600

    Added a user serializer.

--- app/serializers/lineup_serializer.rb
+++ app/serializers/lineup_serializer.rb
@@ -1,3 +1,4 @@
 class LineupSerializer < ActiveModel::Serializer
    attributes :id, :name, :description, :players, :user, :lineups_players
+   belongs_to :user, serializer: LineupUserSerializer
  end
---
+++ app/serializers/lineup_user_serializer.rb
@@ -0,0 +1,3 @@
+class LineupUserSerializer < ActiveModel::Serializer
+  attributes :id, :email
+end
