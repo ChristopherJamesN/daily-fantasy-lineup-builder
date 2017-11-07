d30bc6349eaa2217d3ecb5e772a7e8f6954fdcbc
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-07 07:59:34 -0600

    Removed unused relationships from player serializer.

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,5 +1,3 @@
 class PlayerSerializer < ActiveModel::Serializer
    attributes :id, :name, :position, :projectedPoints, :actualPoints, :lineups
-   has_many :lineups
-   has_many :lineups_players
  end
