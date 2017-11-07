da77f68bb5bce9729ce97a4ffadb8d48b1e77531
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-06 22:06:27 -0600

    Add lineup players to lineup serializer.

--- app/serializers/lineup_serializer.rb
+++ app/serializers/lineup_serializer.rb
@@ -1,5 +1,5 @@
 class LineupSerializer < ActiveModel::Serializer
-   attributes :id, :name, :description, :players, :user
+   attributes :id, :name, :description, :players, :user, :lineups_players
    has_many :players
    belongs_to :user
  end
