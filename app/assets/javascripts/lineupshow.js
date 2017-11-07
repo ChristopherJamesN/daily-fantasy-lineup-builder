34f8ccb5b8767085251ebf744b5794084b96b249
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-07 09:40:17 -0600

    Refactor code.

--- app/assets/javascripts/lineupshow.js
+++ app/assets/javascripts/lineupshow.js
@@ -5,10 +5,25 @@ function showLineups() {
       success: function(response) {
         let new_html = ''
         for (var i = 0; i < response.data.length; i++) {
+
+          var sortedPlayers = response.data[i].attributes.players.sort(function(a,b) {
+            var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+            var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+              if (nameA < nameB) {
+                return -1;
+              }
+              if (nameA > nameB) {
+                return 1;
+              }
+
+              // names must be equal
+              return 0;
+          })
+
           let player_html = ''
-            for (var j = 0; j < response.data[i].attributes.players.length; j++) {
-              player_html += response.data[i].attributes.players[j].name + ' ' + response.data[i].attributes.players[j].position +'<br>'
-            }
+          for (var j = 0; j < sortedPlayers.length; j++) {
+            player_html += sortedPlayers[j].name + ' ' + sortedPlayers[j].position +'<br>'
+          }
           new_html += response.data[i].attributes.name + '<br>' + response.data[i].attributes.description + '<br>' + player_html + '<br>'
         }
         $("#lineups").html(new_html);
