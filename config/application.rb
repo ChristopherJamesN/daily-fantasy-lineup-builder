b8bc5ec1c6aa9804e205be99ee9cb48f75e1316b
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-08 17:58:52 -0600

    Fix typo in application.rb.

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,7 +10,7 @@ module RailsDailyFantasyLineupBuilder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
