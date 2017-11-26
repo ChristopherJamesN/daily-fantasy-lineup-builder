0189b419632775fd07ad5a25ef7cc247e61ab708
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-26 09:04:26 -0600

    Add fields for all draftkings positions.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105164939) do
+ActiveRecord::Schema.define(version: 20171110195054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171105164939) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "link"
   end
 
   create_table "players", force: :cascade do |t|
