b4c605394e128b4eb879dd8ea56feb5340208f3e
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-26 20:36:58 -0600

    Add actual points to seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,30 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #Quarterbacks
-Player.create(name: 'Russell Wilson', position: 'Quarterback', salary: 7000)
-Player.create(name: 'Tom Brady', position: 'Quarterback', salary: 7700)
-Player.create(name: 'Marcus Mariota', position: 'Quarterback', salary: 6300)
-Player.create(name: 'Matt Ryan', position: 'Quarterback', salary: 6400)
+Player.create(name: 'Russell Wilson', position: 'Quarterback', salary: 7000, actualPoints: 25)
+Player.create(name: 'Tom Brady', position: 'Quarterback', salary: 7700, actualPoints: 22)
+Player.create(name: 'Marcus Mariota', position: 'Quarterback', salary: 6300, actualPoints: 10)
+Player.create(name: 'Matt Ryan', position: 'Quarterback', salary: 6400, actualPoints: 15)
 
 #Runningbacks
-Player.create(name: 'Tevin Coleman', position: 'Runningback', salary: 6500)
-Player.create(name: 'Mark Ingram', position: 'Runningback', salary: 8300)
-Player.create(name: 'Todd Gurley', position: 'Runningback', salary: 8800)
-Player.create(name: 'Kareem Hunt', position: 'Runningback', salary: 8000)
+Player.create(name: 'Tevin Coleman', position: 'Runningback', salary: 6500, actualPoints: 21)
+Player.create(name: 'Mark Ingram', position: 'Runningback', salary: 8300, actualPoints: 6)
+Player.create(name: 'Todd Gurley', position: 'Runningback', salary: 8800, actualPoints: 16)
+Player.create(name: 'Kareem Hunt', position: 'Runningback', salary: 8000, actualPoints: 4)
 
 #Widerecievers
-Player.create(name: 'Julio Jones', position: 'Widereciever', salary: 7700)
-Player.create(name: 'Brandin Cooks', position: 'Widereciever', salary: 7100)
-Player.create(name: 'Sammy Watkins', position: 'Widereciever', salary: 4900)
-Player.create(name: 'AJ Green', position: 'Widereciever', salary: 8000)
-Player.create(name: 'Cooper Kupp', position: 'Widereciever', salary: 5000)
+Player.create(name: 'Julio Jones', position: 'Widereciever', salary: 7700, actualPoints: 52)
+Player.create(name: 'Brandin Cooks', position: 'Widereciever', salary: 7100, actualPoints: 21)
+Player.create(name: 'Sammy Watkins', position: 'Widereciever', salary: 4900, actualPoints: 18)
+Player.create(name: 'AJ Green', position: 'Widereciever', salary: 8000, actualPoints: 9)
+Player.create(name: 'Cooper Kupp', position: 'Widereciever', salary: 5000, actualPoints: 22)
 
 #Tightends
-Player.create(name: 'Jack Doyle', position: 'Tightend', salary: 4500)
-Player.create(name: 'Travis Kelce', position: 'Tightend', salary: 7300)
-Player.create(name: 'Delanie Walker', position: 'Tightend', salary: 5000)
+Player.create(name: 'Jack Doyle', position: 'Tightend', salary: 4500, actualPoints: 16)
+Player.create(name: 'Travis Kelce', position: 'Tightend', salary: 7300, actualPoints: 6)
+Player.create(name: 'Delanie Walker', position: 'Tightend', salary: 5000, actualPoints: 16)
 
 #Defense
-Player.create(name: 'Jacksonville', position: 'Defense', salary: 4100)
-Player.create(name: 'Philadelphia', position: 'Defense', salary: 3600)
-Player.create(name: 'Cincinnati', position: 'Defense', salary: 3900)
+Player.create(name: 'Jacksonville', position: 'Defense', salary: 4100, actualPoints: 2)
+Player.create(name: 'Philadelphia', position: 'Defense', salary: 3600, actualPoints: 12)
+Player.create(name: 'Cincinnati', position: 'Defense', salary: 3900, actualPoints: 6)
