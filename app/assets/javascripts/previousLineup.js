81d2c98371f5cdaaedeed96194ac12bf6a2cbc55
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-26 22:23:17 -0600

    Update next and previous scripts to properly calculate the total points.

--- app/assets/javascripts/previousLineup.js
+++ app/assets/javascripts/previousLineup.js
@@ -13,9 +13,14 @@ function previousLineup() {
          + '</td><td>' + response['data']['attributes']['lineups-players'][i]['player_starting'] + '</td></tr>'
        }
 
+       var sum = 0
+       for (var i = 0; i < response.data.attributes.players.length; i++) {
+         sum += response.data.attributes.players[i].actualPoints
+       }
+
        $("#name").html('<strong class="col-sm-4">Name:</strong>' + response.data.attributes.name)
        $("#description").html('<strong class="col-sm-4">Description:</strong>' + response.data.attributes.description)
-       $("#total_points").html('<strong class="col-sm-4">Total Points:</strong>' )
+       $("#total_points").html('<strong class="col-sm-4">Total Points:</strong>' + sum)
        $("#user").html(response.data.attributes.user.id)
        $("#next").attr('data-id', response.data.id)
        $("#previous").attr('data-id', response.data.id)
