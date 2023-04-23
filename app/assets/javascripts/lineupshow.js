03e7f12cbca2f0a05c351e1bf0cc7227d3647e75
ChristopherJamesN <nady.christopher@gmail.com>
2023-04-22 20:50:55 -0500

    Format lineupshow.js and change var to let

--- app/assets/javascripts/lineupshow.js
+++ app/assets/javascripts/lineupshow.js
@@ -1,32 +1,41 @@
 function showLineups() {
-   $.ajax({
-      method: 'GET',
-      url: '/lineups.json',
-      success: function(response) {
-        var new_html = ''
-        for (var i = 0; i < response.data.length; i++) {
-
-          var sortedPlayers = response.data[i].attributes.players.sort(function(a,b) {
-            var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-            var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-              if (nameA < nameB) {
-                return -1;
-              }
-              if (nameA > nameB) {
-                return 1;
-              }
-
-              // names must be equal
-              return 0;
-          })
-
-          var player_html = ''
-          for (var j = 0; j < sortedPlayers.length; j++) {
-            player_html += sortedPlayers[j].name + ' ' + sortedPlayers[j].position +'<br>'
+  $.ajax({
+    method: 'GET',
+    url: '/lineups.json',
+    success: function (response) {
+      let new_html = '';
+      for (let i = 0; i < response.data.length; i++) {
+        let sortedPlayers = response.data[i].attributes.players.sort(function (
+          a,
+          b
+        ) {
+          let nameA = a.name.toUpperCase(); // ignore upper and lowercase
+          let nameB = b.name.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
           }
-          new_html += response.data[i].attributes.name + '<br>' + response.data[i].attributes.description + '<br>' + player_html + '<br>'
+          if (nameA > nameB) {
+            return 1;
+          }
+
+          // names must be equal
+          return 0;
+        });
+
+        let player_html = '';
+        for (let j = 0; j < sortedPlayers.length; j++) {
+          player_html +=
+            sortedPlayers[j].name + ' ' + sortedPlayers[j].position + '<br>';
         }
-        $("#lineups").html(new_html);
+        new_html +=
+          response.data[i].attributes.name +
+          '<br>' +
+          response.data[i].attributes.description +
+          '<br>' +
+          player_html +
+          '<br>';
       }
-    });
-  }
+      $('#lineups').html(new_html);
+    },
+  });
+}
